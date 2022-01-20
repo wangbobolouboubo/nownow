97bdf120b487f33b6c9c151ffe94dcbe8d832699
wangbobolouboubo <wangbobolouboubo@gmail.com>
2022-01-20 19:01:14 +0800

    更护用户之后

---
+++ one/oneday.py
@@ -0,0 +1,11 @@
+# 1.添加
+# $ git config --global user.name "yourName"
+# $ git config --global user.email "your@email.com"
+#
+# 2.删除
+# git config --global --unset user.name "yourName"
+# git config --global --unset user.name "yourName"
+#
+# 看添加或删除是否成功，有两种方式：
+# 【1】是控制台回车后没有报错，即成功。
+# 【2】C盘 --> use(或用户) --> 电脑用户 --> .gitconfig;就可以查看。
